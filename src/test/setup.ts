4bc689075e71f3f4347ef318391dd3f588f1c246
mohamedwahba1 <mo@baianat.net>
2026-06-07 16:37:41 +0300

    test: guard matchMedia mock against non-browser environments

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,1 +1,17 @@
 import "@testing-library/jest-dom";
+
+if (typeof window !== "undefined") {
+  Object.defineProperty(window, "matchMedia", {
+    writable: true,
+    value: (query: string): MediaQueryList => ({
+      matches: false,
+      media: query,
+      onchange: null,
+      addListener: () => {},
+      removeListener: () => {},
+      addEventListener: () => {},
+      removeEventListener: () => {},
+      dispatchEvent: () => false,
+    }),
+  });
+}
